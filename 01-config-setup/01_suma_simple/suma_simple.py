62a0aeab65c22b2288dbff1837b25bf07e267b0c
MartinaHellersDonadio <martinahellers@gmail.com>
2022-08-18 09:50:09 -0300

    nuevo cambio

--- 01-config-setup/01_suma_simple/suma_simple.py
+++ 01-config-setup/01_suma_simple/suma_simple.py
@@ -1,6 +1,4 @@
-
 def sum4(first_number, second_number, third_number, fourth_number):
-    """
-    Takes four numbers and returns the sum of those numbers
-    """
-    return first_number + second_number + third_number+ fourth_number
+    suma = first_number + second_number + third_number + fourth_number
+    return suma
+
